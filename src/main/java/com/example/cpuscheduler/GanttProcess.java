035f0cb20cd600d633e59fb3a0c418ec43c61f6f
Sameh Ossama Koleid <samehossama@hotmail.com>
2024-04-14 21:01:24 +0200

    Add a class for Gantt chart process

---
+++ src/main/java/com/example/cpuscheduler/GanttProcess.java
@@ -0,0 +1,36 @@
+package com.example.cpuscheduler;
+
+public class GanttProcess {
+    private int pid;
+    private int runningTime;
+
+
+    public GanttProcess(int pid, int runningTime) {
+        this.pid = pid;
+        this.runningTime = runningTime;
+    }
+
+    public GanttProcess(Process p, int runningTime) {
+        this(p.getPid(), runningTime);
+    }
+
+    public int getPid() {
+        return pid;
+    }
+
+    public int getRunningTime() {
+        return runningTime;
+    }
+
+    public void setPid(int pid) {
+        this.pid = pid;
+    }
+
+    public void setRunningTime(int runningTime) {
+        this.runningTime = runningTime;
+    }
+
+    public void increment(int quantum) {
+        this.runningTime += quantum;
+    }
+}
