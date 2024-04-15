8887a1ce9ddae69940cd4ad4e0826344ba45d031
farah-salama <farahhsalamaa@gmail.com>
2024-04-16 00:46:44 +0200

    Update HelloController.java

--- src/main/java/com/example/cpuscheduler/HelloController.java
+++ src/main/java/com/example/cpuscheduler/HelloController.java
@@ -1,14 +1,39 @@
 package com.example.cpuscheduler;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
-public class HelloController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ChoiceBox<String> choiceBox;
+
+    static String processType;
+
+    @FXML
+    protected void onDoneButtonClick() throws IOException {
+        processType = choiceBox.getValue();
+        if(processType != null) {
+            ProcessApplication processApplication = new ProcessApplication();
+            Stage processStage = new Stage();
+            processApplication.start(processStage);
+            processStage.show();
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        choiceBox.getItems().addAll("FCFS", "Round Robin", "SJF Preemptive",
+                "SJF Non-Preemptive", "Priority Preemptive", "Priority Non-Preemptive");
     }
 }
