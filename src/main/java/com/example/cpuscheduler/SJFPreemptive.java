632731d3aa20cc7032a8e5d6e707c6fea4854894
Sameh Ossama Koleid <samehossama@hotmail.com>
2024-04-15 19:16:09 +0200

    Update Preemptive SJF to work with Schedulers class

--- src/main/java/com/example/cpuscheduler/SJFPreemptive.java
+++ src/main/java/com/example/cpuscheduler/SJFPreemptive.java
@@ -1,39 +1,15 @@
 package com.example.cpuscheduler;
 
-import java.util.ArrayList;
 import java.util.PriorityQueue;
 
-public class SJFPreemptive {
-    PriorityQueue<Process> queue;
-    ArrayList<GanttProcess> ganttChart;
-    ArrayList<Process> completedProcesses;
-
+public class SJFPreemptive extends Schedulers{
     public SJFPreemptive() {
-        this.queue = new PriorityQueue<>((p1, p2) -> p1.getRemainingTime() - p2.getRemainingTime());
-        this.ganttChart = new ArrayList<GanttProcess>();
-        this.completedProcesses = new ArrayList<Process>();
-    }
-
-    public void enqueue(Process p) {
-        queue.add(p);
-    }
-
-    public void updateGanttChart(Process p, int quantum) {
-        if (!ganttChart.isEmpty()) {
-            GanttProcess lastProcess = ganttChart.get(ganttChart.size() - 1);
-            if (lastProcess.getPid() == p.getPid()) {
-                lastProcess.increment(quantum);
-                return;
-            }
-        }
-
-        ganttChart.add(new GanttProcess(p, quantum));
+        super(new PriorityQueue<Process>((p1, p2) -> p1.getRemainingTime() - p2.getRemainingTime()));
     }
 
     public boolean fetchNextTask(int time) {
         Process running = queue.poll();
         if (running == null) {
-            // updateGanttChart(new Process(-1, -1, -1), 1);
             return false;
         }
     
@@ -53,13 +29,5 @@ public class SJFPreemptive {
 
         queue.add(running);
         return true;
-    }
-
-    public ArrayList<GanttProcess> getGanttChart() {
-        return ganttChart;
-    }
-
-    public ArrayList<Process> getCompletedProcesses() {
-        return completedProcesses;
     }
 }
