cebfc229e32f74ef5912afbb91a12c0e74a6a657
Mariam Mohamed Abdelkader <131121527+MariammAbdelkader@users.noreply.github.com>
2024-04-15 21:42:37 +0200

    Add files via upload

---
+++ src/main/java/com/example/cpuscheduler/FirstComeFirstServe.java
@@ -0,0 +1,33 @@
+package com.example.cpuscheduler;
+
+import java.util.LinkedList;
+
+public class FirstComeFirstServe extends Schedulers{
+    public FirstComeFirstServe() {
+        super(new LinkedList<Process>());
+    }
+
+    public boolean fetchNextTask(int time) {
+        Process running = queue.peek();
+        if (running == null) {
+            return false;
+        }
+
+        if (running.getStartTime() == -1) {
+            running.setStartTime(time);
+        }
+
+        running.decrement(1);
+        updateGanttChart(running, 1);
+        running.setLastProcessedTime(time + 1);
+
+        if (running.getRemainingTime() == 0) {
+            running.setCompletionTime(time + 1);
+            completedProcesses.add(queue.poll());
+            return true;
+        }
+
+       // queue.add(running);
+        return true;
+    }
+}
