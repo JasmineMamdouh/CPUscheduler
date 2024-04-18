2150058a30801bf5b00d2325c615dbdff3ebd701
Sameh Ossama Koleid <samehossama@hotmail.com>
2024-04-18 17:19:30 +0200

    Add the ability to draw Gantt chart once per second

--- src/main/java/com/example/cpuscheduler/GanttApplication.java
+++ src/main/java/com/example/cpuscheduler/GanttApplication.java
@@ -1,6 +1,7 @@
 package com.example.cpuscheduler;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
@@ -12,12 +13,27 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.PriorityQueue;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 
 
-public class GanttApplication extends Application {
+public class GanttApplication extends Application implements Runnable {
     private PriorityQueue<Process> processes;
     HashMap<Integer, Color> colors;
+    ArrayList<GanttProcess> ganttChart;
+    ScheduledFuture<?> t;
+
+    Pane layout;
+    Text timeText;
+    int time = 0;
+    int lastx = 20;
+    int y = 200;
+    RR scheduler = new RR(3);
+    Color color;
+    int current_pid, last_pid = -1;
 
     GanttApplication(PriorityQueue<Process> processes, HashMap<Integer, Color> colors){
         this.processes = processes;
@@ -25,76 +41,92 @@ public class GanttApplication extends Application {
     }
 
     @Override
+    public void run() {
+        try {
+            Platform.runLater(new Runnable() {
+                @Override
+                public void run() {
+                    while (!processes.isEmpty() && time == processes.peek().getArrivalTime()) {
+                            scheduler.enqueue(processes.poll());
+                        }
+
+                        boolean running = scheduler.fetchNextTask(time);
+
+                        // Queue and scheduler have finished serving all processes
+                        timeText.setText("Time = " + time + "second");
+                        
+                        int length = 30;
+                        int width = 20;
+                        Text text;
+
+                        if (!running && processes.isEmpty()) {
+                            text = new Text(time + "");
+                            text.setX(lastx);
+                            text.setY(y + width + 20);
+                            text.setFill(Color.DARKBLUE);
+                            layout.getChildren().add(text);
+                            t.cancel(false);
+                            return;
+                        }
+
+                        ganttChart = scheduler.getGanttChart();
+                        current_pid = ganttChart.get(ganttChart.size() - 1).getPid();
+                        color = colors.get(current_pid);
+                        
+                        // Add time if new process is running
+                        if(current_pid != last_pid) {
+                            last_pid = current_pid;
+                            text = new Text(time + "");
+                            text.setX(lastx);
+                            text.setY(y + width + 20);
+                            text.setFill(Color.DARKBLUE);
+                            layout.getChildren().add(text);
+                        }         
+                        
+                        Rectangle rectangle = new Rectangle(length, width);
+                        rectangle.setX(lastx);
+                        rectangle.setY(y);
+                        lastx += length;
+                        rectangle.setFill(color);
+                    
+                    
+                        layout.getChildren().add(rectangle);
+                        
+                        
+                        time++;
+                            
+                    }
+                });
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+    }
+
+
+    @Override
     public void start(Stage stage) throws IOException {
-        Pane layout = new Pane();
+        layout = new Pane();
         layout.setStyle("-fx-background-color: #EEEEEE");
         Scene scene = new Scene(layout,370,370);
         stage.setTitle("RunLive");
 
         //draw time 0
-        int time = 0;
-        Text timeText = new Text("Time = " + time + "second");
-        int lastx = 20;
-        int y=200;
+        timeText = new Text("Time = " + time + "second");
         timeText.setX(lastx);
         timeText.setY(y);
         layout.getChildren().add(timeText);
 
-        Text text;
-
-        int length=30;
-        int width=20;
-        Color color;
-        RR scheduler = new RR(3);
-
-        boolean running;
-        ArrayList<GanttProcess> ganttChart;
-        int current_pid, last_pid = -1;
-
-        while (true) {
-            while (!processes.isEmpty() && time == processes.peek().getArrivalTime()) {
-                scheduler.enqueue(processes.poll());
-            }
-
-            running = scheduler.fetchNextTask(time);
-
-            // Queue and scheduler have finished serving all processes
-            timeText.setText("Time = " + time + "second");
-
-            if (!running && processes.isEmpty()) {
-                text = new Text(time + "");
-                text.setX(lastx);
-                text.setY(y + width + 20);
-                text.setFill(Color.DARKBLUE);
-                layout.getChildren().add(text);
-                break;
-            }
-
-            ganttChart = scheduler.getGanttChart();
-            current_pid = ganttChart.get(ganttChart.size() - 1).getPid();
-            color = colors.get(current_pid);
-            
-            // Add time if new process is running
-            if(current_pid != last_pid) {
-                last_pid = current_pid;
-                text = new Text(time + "");
-                text.setX(lastx);
-                text.setY(y + width + 20);
-                text.setFill(Color.DARKBLUE);
-                layout.getChildren().add(text);
-            }         
-            
-            Rectangle rectangle = new Rectangle(length, width);
-            rectangle.setX(lastx);
-            rectangle.setY(y);
-            lastx += length;
-            rectangle.setFill(color);
-            layout.getChildren().add(rectangle);
-
-            time++;
-        }
         stage.setScene(scene);
         stage.show();
+        
+        ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+        t = executor.scheduleAtFixedRate(this, 0, 1, TimeUnit.SECONDS);
+
+        // Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(1), e -> run()));
+        // timeline.setCycleCount(Animation.INDEFINITE);
+        // timeline.play();
+
+        
     }
 
 
