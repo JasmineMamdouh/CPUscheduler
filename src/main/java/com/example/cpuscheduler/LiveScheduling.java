75b5b31e35dd214c820fd3d28a36da340b2bf6fc
farah-salama <farahhsalamaa@gmail.com>
2024-04-19 20:05:22 +0200

    add: Add Process

--- src/main/java/com/example/cpuscheduler/LiveScheduling.java
+++ src/main/java/com/example/cpuscheduler/LiveScheduling.java
@@ -1,13 +1,16 @@
 package com.example.cpuscheduler;
 
 import javafx.application.Application;
-import javafx.geometry.Pos;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 
+import javafx.scene.effect.Glow;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -171,14 +174,21 @@ public class LiveScheduling extends Application implements Runnable {
         label.setStyle("-fx-background-color: #E8D5C4; -fx-border-radius: 10px;");
         label.setPrefHeight(40.0);
         label.setFont(new javafx.scene.text.Font(24.0));
+        label.setPrefWidth(350.0);
+        label.setLayoutX(10.0);
+        label.setLayoutY(10.0);
+        Glow glow = new Glow();
+        label.setEffect(glow);
 
         Label label2 = new Label("Gantt Chart");
         label2.setAlignment(javafx.geometry.Pos.CENTER);
         label2.setStyle("-fx-background-color: #E8D5C4; -fx-border-radius: 10px;");
         label2.setPrefHeight(40.0);
         label2.setFont(new javafx.scene.text.Font(24.0));
+        label2.setPrefWidth(350.0);
         label2.setLayoutX(10.0);
-        label2.setLayoutY(420.0);
+        label2.setLayoutY(430.0);
+        label2.setEffect(glow);
 
 
         TableColumn<Process, Integer> pidColumn = new TableColumn<>("PID");
@@ -196,13 +206,15 @@ public class LiveScheduling extends Application implements Runnable {
         table.getColumns().addAll(pidColumn, startColumn, burstColumn, endColumn);
         table.setPrefWidth(370);
         table.setPrefHeight(350);
+        table.setLayoutY(60);
+        table.setLayoutX(10);
         table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
         table.setItems(processesList);
 
         mainLayout.setStyle("-fx-background-color: #EEEEEE");
         mainLayout.getChildren().addAll(label, table,label2);
 
-        Scene scene = new Scene(mainLayout, 700, 620);
+        Scene scene = new Scene(mainLayout, 700, 780);
         stage.setScene(scene);
         stage.setTitle("Scheduling Table");
 
@@ -242,7 +254,64 @@ public class LiveScheduling extends Application implements Runnable {
         avgTurnaroundTime.setFont(new Font(14.0));
         layout.getChildren().add(avgTurnaroundTime);
 
+        layout.setPadding(new Insets(10));
+
+        Label label3 = new Label("New Processes");
+        label3.setAlignment(javafx.geometry.Pos.CENTER);
+        label3.setStyle("-fx-background-color: #E8D5C4; -fx-border-radius: 10px;");
+        label3.setPrefHeight(40.0);
+        label3.setFont(new javafx.scene.text.Font(24.0));
+        label3.setPrefWidth(350.0);
+        label3.setLayoutX(10.0);
+        label3.setLayoutY(430.0);
+        label3.setEffect(glow);
+        mainLayout.getChildren().add(label3);
+
+        Pane addProcessLayout = new Pane();
+
+        TextField pidTextField = new TextField();
+        pidTextField.setLayoutX(10.0);
+        pidTextField.setLayoutY(10.0);
+        pidTextField.setPrefHeight(25.0);
+        pidTextField.setPrefWidth(85.0);
+        pidTextField.setPromptText("PID");
+
+        TextField arrivalTimeTextField = new TextField();
+        arrivalTimeTextField.setLayoutX(120.0);
+        arrivalTimeTextField.setLayoutY(10.0);
+        arrivalTimeTextField.setPrefHeight(25.0);
+        arrivalTimeTextField.setPrefWidth(85.0);
+        arrivalTimeTextField.setPromptText("Arrival Time");
+
+        TextField burstTimeTextField = new TextField();
+        burstTimeTextField.setLayoutX(230.0);
+        burstTimeTextField.setLayoutY(10.0);
+        burstTimeTextField.setPrefHeight(25.0);
+        burstTimeTextField.setPrefWidth(85.0);
+        burstTimeTextField.setPromptText("Burst Time");
+
+        Button addProcessButton = new Button();
+
+        // Set properties
+        addProcessButton.setLayoutX(350.0);
+        addProcessButton.setLayoutY(10.0);
+        addProcessButton.setMnemonicParsing(false);
+        addProcessButton.setPrefHeight(36.0);
+        addProcessButton.setPrefWidth(135.0);
+        addProcessButton.setStyle("-fx-background-color: #3A98B9;");
+        addProcessButton.setText("Add Process");
+        addProcessButton.setTextFill(javafx.scene.paint.Color.WHITE);
+
+
+        addProcessLayout.getChildren().addAll(pidTextField,arrivalTimeTextField,burstTimeTextField, addProcessButton);
+        mainLayout.getChildren().add(addProcessLayout);
+        mainLayout.setPadding(new Insets(10));
+        mainLayout.setSpacing(10);
+
         stage.show();
+    }
+    private void onAddButtonClick(){
+
     }
 
     public static void main(String[] args) {
