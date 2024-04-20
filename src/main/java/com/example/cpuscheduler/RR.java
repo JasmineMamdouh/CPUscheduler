4380c790a4076ee6a2f8b3e476a3373d93f0840d
Sameh Ossama Koleid <samehossama@hotmail.com>
2024-04-20 22:12:25 +0200

    Update round robin scheduling algorithm

--- src/main/java/com/example/cpuscheduler/RR.java
+++ src/main/java/com/example/cpuscheduler/RR.java
@@ -1,45 +1,47 @@
 package com.example.cpuscheduler;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.Queue;
 
 public class RR extends Schedulers {
-    int quantum; //RR timeslice
-    int itr=1;// iterator to keep track if the timeslice finished or not.
+    private int quantum; //RR timeslice
+    private int itr=1;// iterator to keep track if the timeslice finished or not.
+    Process running;
 
     public RR(int quantum){
-        super(new LinkedList());
+        // super(new PriorityQueue<>(Comparator.comparing(Process::getLastProcessedTime).reversed()));
+        super(new LinkedList<>());
         this.quantum=quantum; //time slice for round robin
     }
 
     public boolean fetchNextTask(int time) {
-        Process running = queue.peek(); //acessing head of Queue
-        if (running == null) {
+        if (itr == quantum) { // if timeslice finished
+            queue.add(running); // remove the process from head of queue and add it at the end
+            itr = 0; // reset itr
+            running = null;
+        }
+        if (running == null) { // if last task is completed, get next task
+            running = queue.poll();
+        }
+        if (running == null && queue.peek() == null) {
             updateGanttChart(-1, 1);
-            return false; //return false if queue is empty
+            return false;// return false if queue is empty
         }
         if (running.getStartTime() == -1) {
             running.setStartTime(time); //set starting time of process once it starts
         }
         running.decrement(1); //run the process for 1 unit time
         updateGanttChart(running, 1); //update gantt chart
+        running.setLastProcessedTime(time+1);
 
         if (running.getRemainingTime() == 0) { //in case process is done
             running.setCompletionTime(time+1); //set completion time to (time+1)
-           // running.setWaitingTime((time+1)- running.getArrivalTime()- running.getBurstTime());
-            completedProcesses.add(queue.poll()); //add process to completedProcess Queue
-            itr=1;//reset itr
+            completedProcesses.add(running); //add process to completedProcess Queue
+            running=null;
+            itr=0;//reset itr
             return true;
         }
 
-        if(itr== quantum){ //if timeslice finished
-            queue.add(queue.poll()); //remove the process from head of queue and add it at the end
-            itr=1; //reset itr
-            return true;
-        }
-
-        else itr++; //increment itr
+        itr++; //increment itr
         return true;
     }
 
